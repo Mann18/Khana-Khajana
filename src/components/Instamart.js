16680615684192a59d861c53b311c0ea69bf0d8a
Manan Patel <manan28112002@gmail.com>
2023-08-09 12:24:09 +0530

    updated accordion

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -3,37 +3,34 @@ import { useState } from "react";
 const Section = ({ title, description, isVisible, setIsVisible }) => {
   return (
     <div className="border border-black p-2 m-2">
-      <h1 className=" font-bold text-xl">{title}</h1>
+      {/* <h1 className=" font-bold text-xl">{title}</h1> */}
       {isVisible ? (
         <div>
           <button
+            className="font-bold text-xl cursor-pointer underline"
             onClick={() => {
               setIsVisible(false);
             }}
           >
-            hide
+            {title}
           </button>{" "}
           <h1>{description}</h1>
         </div>
       ) : (
         <button
-          className="mr-2"
+          className="font-bold text-xl cursor-pointer underline"
           onClick={() => {
             setIsVisible(true);
           }}
         >
-          show
+          {title}
         </button>
       )}
     </div>
   );
 };
 const Instamart = () => {
-  const [sectionConfig, setSectionConfig] = useState({
-    showAbout: false,
-    showTeam: false,
-    showProduct: false,
-  });
+  const [visibleSection, setVisibleSection] = useState("");
   return (
     <div>
       <h1 className="text-3xl p-2 m-2 font-bold">Instamart Page</h1>
@@ -41,39 +38,27 @@ const Instamart = () => {
         title="About Instamart"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
-        isVisible={sectionConfig.showAbout}
+        isVisible={visibleSection === "about"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: true,
-            showTeam: false,
-            showProduct: false,
-          })
+          setVisibleSection(visibleSection === "about" ? "" : "about")
         }
       />
       <Section
         title="Team Instamart"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
-        isVisible={sectionConfig.showTeam}
+        isVisible={visibleSection === "team"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: true,
-            showProduct: false,
-          })
+          setVisibleSection(visibleSection === "team" ? "" : "team")
         }
       />
       <Section
         title="Products"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
-        isVisible={sectionConfig.showProduct}
+        isVisible={visibleSection == "product"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: false,
-            showProduct: true,
-          })
+          setVisibleSection(visibleSection === "product" ? "" : "product")
         }
       />
     </div>
