8b456ccf2d895958bed67680fe60611f3c773bc7
Manan Patel <manan28112002@gmail.com>
2023-08-09 11:17:24 +0530

    lifting the state up-1

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
-const Section = ({ type, description }) => {
-  const [isVisible, setIsVisible] = useState(false);
+const Section = ({ title, description, isVisible, setIsVisible }) => {
   return (
     <div className="border border-black p-2 m-2">
-      <h1 className=" font-bold text-xl">{type}</h1>
+      <h1 className=" font-bold text-xl">{title}</h1>
       {isVisible ? (
         <div>
           <button
@@ -30,23 +29,52 @@ const Section = ({ type, description }) => {
   );
 };
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout: false,
+    showTeam: false,
+    showProduct: false,
+  });
   return (
     <div>
       <h1 className="text-3xl p-2 m-2 font-bold">Instamart Page</h1>
       <Section
-        type="About Instamart"
+        title="About Instamart"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
+        isVisible={sectionConfig.showAbout}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: true,
+            showTeam: false,
+            showProduct: false,
+          })
+        }
       />
       <Section
-        type="Contact Instamart"
+        title="Team Instamart"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
+        isVisible={sectionConfig.showTeam}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: true,
+            showProduct: false,
+          })
+        }
       />
       <Section
-        type="Products"
+        title="Products"
         description="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo eius odio laboriosam atque exercitationem ducimus qui. Autem, harum sit et magni minima, odio mollitia quod tenetur ipsam velit atque corrupti. Optio voluptate doloremque itaque repellendus voluptatem labore qui aspernatur sequi inventore iusto quos, iste, vero necessitatibus. Incidunt, error quod unde quo architecto consequatur dolor minus accusamus sequi provident excepturi beatae velit vel dignissimos quae numquam aspernatur inventore mollitia minima voluptates corporis molestias repellendus enim ratione! Laboriosam corporis eligendi iste, nisi et dolorum laudantium reiciendis deserunt adipisci nihil beatae, ipsa quod ea assumenda, modi facere repudiandae voluptatibus deleniti neque. Non?
 "
+        isVisible={sectionConfig.showProduct}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: false,
+            showProduct: true,
+          })
+        }
       />
     </div>
   );
